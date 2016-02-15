8a68199295c66d26a96f48b32aeee158276e9cbc
Paul Ladyman <paul.ladyman@bbc.co.uk>
2016-02-15 16:24:32 +0000

    [redis] rerun command after brpopping something

--- redis/day1/homework/sub/index.js
+++ redis/day1/homework/sub/index.js
@@ -1,13 +1,18 @@
 var redis = require("redis"),
     client = redis.createClient({'host':'localhost', 'port':6379});
 
+function subscribe() {
+  client.brpop(['chat', 0], function (err, res) {
+    if (err)
+      throw Error(err);
+
+    console.log(res);
+    subscribe();
+  });
+}
+
 client.on("error", function (err) {
     console.log("Error " + err);
 });
 
-client.brpop(['chat', 300], function (err, res) {
-  if (err)
-    throw Error(err);
-
-  console.log(res);
-});
+subscribe();
